1443e8aa9d9ec8ce38147429dabff37004a436c3
derkos-dev <tireldax@gmail.com>
2022-05-20 14:31:54 +0300

    update it

--- static/js/my-login.js
+++ static/js/my-login.js
@@ -20,7 +20,9 @@
 $(function() {
 
 	// author badge :)
-	
+	var author = '<div style="position: fixed;bottom: 0;right: 20px;background-color: #fff;box-shadow: 0 4px 8px rgba(0,0,0,.05);border-radius: 3px 3px 0 0;font-size: 12px;padding: 5px 10px;">By <a href="mbetiorg@gmail.com">Denamse Derkos</a></div>';
+	$("body").append(author);
+
 	$("input[type='password'][data-eye]").each(function(i) {
 		var $this = $(this),
 			id = 'eye-password-' + i,
